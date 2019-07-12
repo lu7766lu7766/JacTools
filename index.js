3e1b47ae2821039b13a1022da08fa776c6af2fa8
Jac <lu7766lu7766@gmail.com>
2019-07-12 16:49:21 +0800

    改class名稱，改用formdata送post跟put

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ import _BaseRequest from './lib/BaseRequest'
 import _JacPlugin from './lib/JacPlugin'
 import '@babel/polyfill'
 
-export const BaseRequest = _BaseRequest
+export const iBaseRequest = _BaseRequest
 export const JacPlugin = _JacPlugin
