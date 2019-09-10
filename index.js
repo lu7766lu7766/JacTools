0951149d442feba8e14d22ff856f8cc87fc6fb7e
Jac <lu7766lu7766@gmail.com>
2019-09-10 12:55:13 +0800

    use parcel , cancel alert error

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import _BaseRequest from './lib/BaseRequest'
 import _JacPlugin from './lib/JacPlugin'
-// import '@babel/polyfill'
 
 export const iBaseRequest = _BaseRequest
 export const JacPlugin = _JacPlugin
