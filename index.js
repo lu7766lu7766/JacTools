44d3e82f7929a595bc67d56bb2964eca1fc82769
Jac <lu7766lu7766@gmail.com>
2019-09-11 19:39:39 +0800

    parcel use again

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-import _BaseRequest from './lib/iBaseRequest'
+import { iBaseRequest as _BaseRequest } from './lib/iBaseRequest'
 import _JacPlugin from './lib/JacPlugin'
 
 export const iBaseRequest = _BaseRequest
 export const JacPlugin = _JacPlugin
 
-// export default {
-//   iBaseRequest: _BaseRequest,
-//   JacPlugin: _JacPlugin
-// }
+
+// import iBaseRequest from './lib/iBaseRequest'
+// import JacPlugin from './lib/JacPlugin'
+
+// export {iBaseRequest, JacPlugin}
